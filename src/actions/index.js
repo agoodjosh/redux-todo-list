8a54f030f09f6ad712dc9d05f4e585ffc8378fd0
jauxsh <joshua.jih.chang@gmail.com>
2018-01-12 12:24:55 -0800

    connected to server done

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,40 @@
 import types from './types';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=jauxshskey';
 
 export function addItem(item) {
+    const request = axios.post(BASE_URL + '/todos' + API_KEY, item);
+
     return {
         type: types.ADD_ITEM,
-        payload: item
+        payload: request
     }
 };
 
+export function getItems() {
+    const request = axios.get(BASE_URL + '/todos' + API_KEY);
+
+    return {
+        type: types.GET_ITEMS,
+        payload: request
+    }
+};
+
+export function getSingleItem(id) {
+    const request = axios.get(BASE_URL + '/todos/' + id + API_KEY);
+
+    return {
+        type: types.GET_SINGLE_ITEM,
+        payload: request
+    }
+}
+
+/*
+
+axios.delete(http://api.reactprototypes.com/todos/[id#]?key=[my api key]);
+
+axios.put(http://api.reactprototypes.com/todos/[id#]?key=[my api key]);
+
+*/
