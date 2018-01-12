1da44f7afad79a3045b508937703aa99541c662d
jauxsh <joshua.jih.chang@gmail.com>
2018-01-11 17:03:17 -0800

    completed add item with dummy data

--- src/reducers/todo_reducer.js
+++ src/reducers/todo_reducer.js
@@ -2,11 +2,22 @@ import types from '../actions/types';
 
 
 const DEFAULT_STATE = {
-    list: []
+    list: [
+        {
+            title: "Make to do List",
+            details: 'Build it in react with redux'
+        },
+        {
+            title: "Fix inappropriate typo",
+            details: 'Fit to redux without an I'
+        }
+    ]
 };
 
 export default function (state = DEFAULT_STATE, action) {
     switch (action.type) {
+        case types.ADD_ITEM:
+            return { list: [action.payload, ...state.list] }
         default:
             return state;
     }
